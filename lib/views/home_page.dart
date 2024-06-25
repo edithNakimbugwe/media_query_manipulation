796c39d02e14ac3055db4ae644f8f52556f1f093
Edith Nakimbugwe <shanitadith@gmail.com>
2024-06-25 14:19:18 +0300

    orientations

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -6,23 +6,55 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var _mediaQuery = MediaQuery.of(context);
+    // return Scaffold(
+    //   body: Row(
+    //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    //     children: [
+    //       Container(
+    //         width: _mediaQuery.size.width * 0.33,
+    //         decoration: const BoxDecoration(color: Colors.black),
+    //       ),
+    //       Container(
+    //         width: _mediaQuery.size.width * 0.333,
+    //         decoration: const BoxDecoration(color: Colors.yellow),
+    //       ),
+    //       Container(
+    //         width: _mediaQuery.size.width * 0.333,
+    //         decoration: const BoxDecoration(color: Colors.red),
+    //       )
+    //     ],
+    //   ),
+    // );
+
     return Scaffold(
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            width: _mediaQuery.size.width * 0.333,
-            decoration: const BoxDecoration(color: Colors.black),
-          ),
-          Container(
-            width: _mediaQuery.size.width * 0.333,
-            decoration: const BoxDecoration(color: Colors.yellow),
-          ),
-          Container(
-            width: _mediaQuery.size.width * 0.333,
-            decoration: const BoxDecoration(color: Colors.red),
-          )
-        ],
+      body: Builder(builder: (context) {
+        if (_mediaQuery.orientation == Orientation.portrait) {
+          return portraitWidget(_mediaQuery.size);
+        } else {
+          return landscapeWidget(_mediaQuery.size);
+        }
+      }),
+    );
+  }
+
+  Widget portraitWidget(Size size) {
+    return Center(
+      child: Container(
+        width: size.width * 0.8,
+        height: size.height * 0.8,
+        decoration: const BoxDecoration(color: Colors.amber),
+        child: const Center(child: Text('Potrait')),
+      ),
+    );
+  }
+
+  Widget landscapeWidget(Size size) {
+    return Center(
+      child: Container(
+        width: size.width * 0.8,
+        height: size.height * 0.8,
+        decoration: const BoxDecoration(color: Colors.blue),
+        child: const Center(child: Text('Landscape')),
       ),
     );
   }
